6854d77a618a2cb0b7f286a12dbe19320ed56a92
Tarun Narayanan <tarunnarayanan.v.2017.cse@rajalakshmi.edu.in>
2021-01-25 13:49:59 +0530

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -8,29 +8,54 @@ import torch
 import pickle
 
 
-def build_loader(DATA_PATH, image_size,batch_size):
+# def build_loader(DATA_PATH, image_size,batch_size):
 
-  transform = transforms.Compose([transforms.ToTensor(), transforms.Resize([image_size, image_size], interpolation=2)])
-  val_data = datasets.ImageFolder(root=DATA_PATH,transform=transform) 
-  dataset_paths = [pair[0] for pair in val_data.samples]
-  val_loader = DataLoader(val_data, batch_size = batch_size, shuffle=False, drop_last=False)
+#   transform = transforms.Compose([transforms.ToTensor(), transforms.Resize([image_size, image_size], interpolation=2)])
+#   val_data = datasets.ImageFolder(root=DATA_PATH,transform=transform) 
+#   dataset_paths = [pair[0] for pair in val_data.samples]
+#   val_loader = DataLoader(val_data, batch_size = batch_size, shuffle=False, drop_last=False)
 
-  return val_loader, dataset_paths
+#   return val_loader, dataset_paths
 
-def get_embeddings(ckpt_path,data_loader, device):
 
-  embedding = []
-  simclr_model = torch.load(ckpt_path)
-  simclr_model.eval()
-  # simclr_model.to(device)
-  for step,(x,y) in enumerate(data_loader):
-    x = x.to(device)
-    # print(simclr_model(x))
-    embedding.extend(simclr_model(x)[0].detach().cpu().numpy())
-  print("Number of Embeddings:", len(embedding))
-  print("Shape of Single Embedding:",embedding[0].shape)
-  print(embedding[0])
-  return embedding
+def get_embeddings_test(DATA_PATH, ckpt, size, embedding_size):
+  ims = []
+  for folder in os.listdir(DATA_PATH):
+    for im in os.listdir(f'{DATA_PATH}/{folder}'):
+      ims.append(f'{DATA_PATH}/{folder}/{im}')
+  
+  model = torch.load(ckpt)
+  model.eval()
+  model.cuda()
+  t = transforms.Resize((size, size))
+  embedding_matrix = torch.empty(size= (0, embedding_size)).cuda()
+  
+  for f in tqdm(ims):
+    with torch.no_grad():
+      im = Image.open(f).convert('RGB')
+      im = t(im)
+      im = np.asarray(im).transpose(2, 0, 1)
+      im = torch.Tensor(im).unsqueeze(0).cuda()
+      embedding = model(im)[0]
+      embedding_matrix = torch.vstack((embedding_matrix, embedding))
+  print('Embedding Shape', embedding_matrix.shape)
+  return embedding_matrix.detach().cpu().numpy()
+
+
+# def get_embeddings(ckpt_path,data_loader, device):
+
+#   embedding = []
+#   simclr_model = torch.load(ckpt_path)
+#   simclr_model.eval()
+#   # simclr_model.to(device)
+#   for step,(x,y) in enumerate(data_loader):
+#     x = x.to(device)
+#     # print(simclr_model(x))
+#     embedding.extend(simclr_model(x)[0].detach().cpu().numpy())
+#   print("Number of Embeddings:", len(embedding))
+#   print("Shape of Single Embedding:",embedding[0].shape)
+#   print(embedding[0])
+#   return embedding
 
 def prepare_tree(num_nodes, features_list_x, path, num_trees):
   t = AnnoyIndex(num_nodes, 'euclidean')
@@ -53,6 +78,7 @@ def main():
   parser.add_argument("--DATA_PATH",type = str, help="Path to image to perform inference")
   parser.add_argument("--ckpt_path",type = str, help="Location of model checkpoint")
   parser.add_argument("--annoy_path",type = str,help="Location to save annoy file (end with .ann)")
+  parser.add_argument("--embedding_size", default= 128, type = int, help="Image size for embedding")
   # parser.add_argument("--embedding_path",type = str, help="Location to save embeddings pickle file")
   parser.add_argument("--device", default = 'cuda', type= str, help="device to run inference on" )
   parser.add_argument("--num_nodes",type = int, help="Number of nodes in the final dense layer of the model")
@@ -67,11 +93,13 @@ def main():
   # embedding_path = args.embedding_path
   num_nodes = args.num_nodes
   batch_size = args.batch_size
+  emb_size = args.embedding_size
   num_trees = args.num_trees
   device = args.device
 
-  data_loader, dataset_paths = build_loader(DATA_PATH, size, batch_size)
-  embedding = get_embeddings(ckpt_path, data_loader, device)
+  embedding = get_embeddings_test(DATA_PATH, ckpt_path, size, emb_size)
+  # data_loader, dataset_paths = build_loader(DATA_PATH, size)
+  # embedding = get_embeddings(ckpt_path, data_loader, device)
   print("Annoy file stored at",prepare_tree(num_nodes, embedding, annoy_path, num_trees = num_trees))
   # print("Embeddings Pickle file stored at",pickle_filepaths(dataset_paths,embedding_path))
 
