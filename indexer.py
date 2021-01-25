1cb91869d2bd1ae3fe709c0cb35e5659b377a178
Tarun Narayanan <tarunnarayanan.v.2017.cse@rajalakshmi.edu.in>
2021-01-25 14:53:18 +0530

    cli tool fixed

--- indexer.py
+++ indexer.py
@@ -13,16 +13,6 @@ import PIL.Image as Image
 from torchvision import transforms
 
 
-# def build_loader(DATA_PATH, image_size,batch_size):
-
-#   transform = transforms.Compose([transforms.ToTensor(), transforms.Resize([image_size, image_size], interpolation=2)])
-#   val_data = datasets.ImageFolder(root=DATA_PATH,transform=transform) 
-#   dataset_paths = [pair[0] for pair in val_data.samples]
-#   val_loader = DataLoader(val_data, batch_size = batch_size, shuffle=False, drop_last=False)
-
-#   return val_loader, dataset_paths
-
-
 def get_embeddings_test(DATA_PATH, ckpt, size, embedding_size):
   ims = []
   for folder in os.listdir(DATA_PATH):
@@ -47,28 +37,12 @@ def get_embeddings_test(DATA_PATH, ckpt, size, embedding_size):
   return embedding_matrix.detach().cpu().numpy()
 
 
-# def get_embeddings(ckpt_path,data_loader, device):
-
-#   embedding = []
-#   simclr_model = torch.load(ckpt_path)
-#   simclr_model.eval()
-#   # simclr_model.to(device)
-#   for step,(x,y) in enumerate(data_loader):
-#     x = x.to(device)
-#     # print(simclr_model(x))
-#     embedding.extend(simclr_model(x)[0].detach().cpu().numpy())
-#   print("Number of Embeddings:", len(embedding))
-#   print("Shape of Single Embedding:",embedding[0].shape)
-#   print(embedding[0])
-#   return embedding
-
 def prepare_tree(num_nodes, features_list_x, path, num_trees):
   t = AnnoyIndex(num_nodes, 'euclidean')
   for i in range(len(features_list_x)):
     t.add_item(i,features_list_x[i])
   t.build(num_trees)
   t.save(path)
-  # print("Tree saved at:",path)
   return path
 
 def pickle_filepaths(dataset_paths,pickle_path):
@@ -84,7 +58,6 @@ def main():
   parser.add_argument("--ckpt_path",type = str, help="Location of model checkpoint")
   parser.add_argument("--annoy_path",type = str,help="Location to save annoy file (end with .ann)")
   parser.add_argument("--embedding_size", default= 128, type = int, help="Image size for embedding")
-  # parser.add_argument("--embedding_path",type = str, help="Location to save embeddings pickle file")
   parser.add_argument("--device", default = 'cuda', type= str, help="device to run inference on" )
   parser.add_argument("--num_nodes",type = int, help="Number of nodes in the final dense layer of the model")
   parser.add_argument("--num_trees",default = 50, type = int, help="Number of trees in Annoy")
@@ -95,7 +68,6 @@ def main():
   size = args.image_size
   ckpt_path = args.ckpt_path
   annoy_path = args.annoy_path
-  # embedding_path = args.embedding_path
   num_nodes = args.num_nodes
   batch_size = args.batch_size
   emb_size = args.embedding_size
@@ -103,10 +75,7 @@ def main():
   device = args.device
 
   embedding = get_embeddings_test(DATA_PATH, ckpt_path, size, emb_size)
-  # data_loader, dataset_paths = build_loader(DATA_PATH, size)
-  # embedding = get_embeddings(ckpt_path, data_loader, device)
   print("Annoy file stored at",prepare_tree(num_nodes, embedding, annoy_path, num_trees = num_trees))
-  # print("Embeddings Pickle file stored at",pickle_filepaths(dataset_paths,embedding_path))
 
 if __name__ == "__main__":
   main()
